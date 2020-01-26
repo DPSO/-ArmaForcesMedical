7e91eca0d4f3a5cbfb615612ef98c580fe3168c6
unknown <mydlo333@interia.pl>
2020-01-26 22:41:50 +0100

    Initial commit

---
+++ addons/main/script_mod.hpp
@@ -0,0 +1,22 @@
+#define MAINPREFIX z
+#define PREFIX afmed
+
+#include "script_version.hpp"
+
+#define VERSION     MAJOR.MINOR
+#define VERSION_STR MAJOR.MINOR.PATCH.BUILD
+#define VERSION_AR  MAJOR,MINOR,PATCH,BUILD
+
+#define REQUIRED_VERSION 1.94
+
+#ifdef COMPONENT_BEAUTIFIED
+    #define COMPONENT_NAME QUOTE(AFMED - COMPONENT_BEAUTIFIED)
+#else
+    #define COMPONENT_NAME QUOTE(AFMED - COMPONENT)
+#endif
+
+#ifdef DISABLE_COMPILE_CACHE
+    #define LINKFUNC(x) {_this call FUNC(x)}
+#else
+    #define LINKFUNC(x) FUNC(x)
+#endif
---
+++ mod.cpp
@@ -0,0 +1,8 @@
+name = "ArmaForces Medical - 0.0.0";
+dir = "@armaforces_medical";
+author = "ArmaForces";
+url = "https://github.com/armaforces/medical/";
+action = "https://armaforces.com/";
+actionName = "Website";
+tooltip = "ArmaForces Medical";
+description = "ArmaForces Medical developed by 3Mydlo3.";
